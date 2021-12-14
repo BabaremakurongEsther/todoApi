f7bdb4dc9725095cd787e694d8247d456b156afb
unknown <babaremakurong@gmail.com>
2021-12-14 10:41:00 +0000

    changes made

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
 import express from "express";
 import dotenv from "dotenv";
-import {connectDB} from "./db/connectdb.js";
-import {Todo} from"./schema/todoSchema.js";
-import cors from 'cors'
+import { connectDB } from "./db/connectdb.js";
+import { Todo } from "./schema/todoSchema.js";
+import cors from "cors";
 
 dotenv.config();
-app.use (cors())
 
 const app = express();
 const port = process.env.PORT || 6000;
+app.use(cors());
+
 
 connectDB();
 
-
-app.get("/", async(req, res) => {
-const todos= await Todo.find();
-if(todos){
-  return res.status(300).json({
-    success:true,
-    data:todos,
-    message:"Todos retrived successfully",
-  });
-}else {
-  return res.status(500).json({
-    success:false,
-    message: "Todos not retrived",
-  });
-}
-
+app.get("/", async (req, res) => {
+  const todos = await Todo.find();
+  if (todos) {
+    return res.status(200).json({
+      success: true,
+      data: todos,
+      message: "Todos retrived successfully",
+    });
+  } else {
+    return res.status(500).json({
+      success: false,
+      message: "Todos not retrived",
+    });
+  }
 });
 
 app.use(express.json());
 
-app.post("/todos", async(req,res)=>{
-  const {title, description,date_time}=req.body;
- const todo =await Todo.create({
-   title, description,date_time,
- });
- if(todo){
-
-   return res.status(2021).json({
-     success:true,
-     data:todo,
-   })
- }else{
-   return res.status(400).json({
-     success:false,
-     message:"Todo not created",
-   })
- }
+app.post("/todos", async (req, res) => {
+  const { title, description, date_time } = req.body;
+  const todo = await Todo.create({
+    title,
+    description,
+    date_time,
+  });
+  if (todo) {
+    return res.status(200).json({
+      success: true,
+      data: todo,
+    });
+  } else {
+    return res.status(400).json({
+      success: false,
+      message: "Todo not created",
+    });
+  }
 });
 
 app.listen(port, () => {
@@ -56,7 +56,7 @@ app.listen(port, () => {
 });
 
 app.get("/todos", (req, res) => {
-  res.json([{name: "abc",age:23}]);
+  res.json([{ name: "abc", age: 23 }]);
 });
 
 app.patch("/todo:id", (req, res) => {
